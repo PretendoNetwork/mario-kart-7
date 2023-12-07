60658ca8943d35c7818a9636f80bbe17184b5e07
Daniel López Guimaraes <danielectra@outlook.com>
2023-12-07 14:36:47 +0000

    Update with latest changes

--- nex/register_common_authentication_server_protocols.go
+++ nex/register_common_authentication_server_protocols.go
@@ -2,7 +2,6 @@ package nex
 
 import (
 	"os"
-	"strconv"
 
 	nex "github.com/PretendoNetwork/nex-go"
 	ticket_granting "github.com/PretendoNetwork/nex-protocols-common-go/ticket-granting"
@@ -12,17 +11,15 @@ import (
 func registerCommonAuthenticationServerProtocols() {
 	ticketGrantingProtocol := ticket_granting.NewCommonTicketGrantingProtocol(globals.AuthenticationServer)
 
-	port, _ := strconv.Atoi(os.Getenv("PN_MK7_SECURE_SERVER_PORT"))
-
 	secureStationURL := nex.NewStationURL("")
-	secureStationURL.SetScheme("prudps")
-	secureStationURL.SetAddress(os.Getenv("PN_MK7_SECURE_SERVER_HOST"))
-	secureStationURL.SetPort(uint32(port))
-	secureStationURL.SetCID(1)
-	secureStationURL.SetPID(nex.NewPID[uint32](2))
-	secureStationURL.SetSID(1)
-	secureStationURL.SetStream(10)
-	secureStationURL.SetType(2)
+	secureStationURL.Scheme = "prudps"
+	secureStationURL.Fields.Set("address", os.Getenv("PN_MK7_SECURE_SERVER_HOST"))
+	secureStationURL.Fields.Set("port", os.Getenv("PN_MK7_SECURE_SERVER_PORT"))
+	secureStationURL.Fields.Set("CID", "1")
+	secureStationURL.Fields.Set("PID", "2")
+	secureStationURL.Fields.Set("sid", "1")
+	secureStationURL.Fields.Set("stream", "10")
+	secureStationURL.Fields.Set("type", "2")
 
 	ticketGrantingProtocol.SetSecureStationURL(secureStationURL)
 	ticketGrantingProtocol.SetBuildName(serverBuildString)
